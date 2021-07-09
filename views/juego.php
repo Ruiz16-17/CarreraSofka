2389daa7225ffe8f2274afc5571bddfef6ee1925
Ruiz16-17 <83104318+Ruiz16-17@users.noreply.github.com>
2021-07-09 15:24:57 -0500

    Update juego.php
    Animate dice

--- views/juego.php
+++ views/juego.php
@@ -8,6 +8,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/estilos.css">
 
     <title>Carrera</title>
 
@@ -37,6 +38,8 @@
                 <th scope="col">Auto</th>
                 <th scope="col">Carril</th>
                 <th scope="col">Recorrido en metros</th>
+                <th scope="col">Lanzar</th>
+                <th scope="col">Avanzar</th>
             </tr>
         </thead>
         <tbody>
@@ -50,22 +53,32 @@
                         <td><?php echo $fila['color'] ?></td>
                         <td><?php echo $fila['id'] ?></td>
                         <td><?php echo $fila['desplazamiento'] ?></td>
-                        <td>
 
-                            <input type="hidden" id="txtRecorrido<?php echo $fila['id'] ?>" name="txtRecorrido<?php echo $fila['id'] ?>">
-                            <input type="hidden" value="<?php echo $fila['id'] ?>" name="txtIdCarril">
-                            <input type="hidden" value="<?php echo $fila['id_Jugador'] ?>" name="txtIdJugador">
-                            <input type="hidden" value="<?php echo ($carrera[0]['km'] * 1000) ?>" name="txtMeta">
-                            <input type="hidden" value="<?php echo $_GET['idPodio'] ?>" name="txtIdPodio">
-
-                        </td>
                         <?php
                         if ($terminar != 0) {
                             if (($carrera[0]['km'] * 1000) > $fila['desplazamiento']) {
                                 if ($fila['turno'] == 1) {
                         ?>
                                     <td>
-                                        <button type="button" name="btnLanzar" class="btn btn-warning" onclick="lanzar(<?php echo $fila['id'] ?>,'btnAvanzar');deshabilitar('btnLanzar<?php echo $fila['id'] ?>');" id="btnLanzar<?php echo $fila['id'] ?>">Lanzar</button>
+                                        <button type="button" name="btnLanzar" class="btn btn-warning lanzar" onclick="lanzar(<?php echo $fila['id'] ?>,'btnAvanzar');deshabilitar('btnLanzar<?php echo $fila['id'] ?>');" id="btnLanzar<?php echo $fila['id'] ?>">
+                                            <input type="hidden" id="txtRecorrido<?php echo $fila['id'] ?>" name="txtRecorrido<?php echo $fila['id'] ?>">
+                                            <input type="hidden" value="<?php echo $fila['id'] ?>" name="txtIdCarril">
+                                            <input type="hidden" value="<?php echo $fila['id_Jugador'] ?>" name="txtIdJugador">
+                                            <input type="hidden" value="<?php echo ($carrera[0]['km'] * 1000) ?>" name="txtMeta">
+                                            <input type="hidden" value="<?php echo $_GET['idPodio'] ?>" name="txtIdPodio">
+
+                                            <i class="bi bi-dice-6 dado">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-dice-6" viewBox="0 0 16 16">
+                                                    <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+                                                    <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                                                </svg>
+                                            </i>
+                                            <i class="bi bi-dice-6-fill">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-dice-6-fill" viewBox="0 0 16 16">
+                                                    <path d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3zm1 5.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm8 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm1.5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM12 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM4 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                                                </svg>
+                                            </i>
+                                        </button>
 
                                     </td>
 
@@ -97,7 +110,7 @@
     ?>
         <div>
 
-            <a href="./ganadores.php?idPodio=<?php echo $_GET['idPodio']?>">
+            <a href="./ganadores.php?idPodio=<?php echo $_GET['idPodio'] ?>">
 
                 <h2>Terminar juego</h2>
 
@@ -109,13 +122,20 @@
     }
 
     ?>
+    <div class="puntaje">
+        <div class="numeroDado">
+            <h2>Sacaste</h2>
+            <h2 name="txtDado" id="txtDado"></h2>
 
-    <div>
-        <h2>Sacaste</h2>
-        <h2 name="txtDado" id="txtDado"></h2>
-        <h2>Avanzas</h2>
-        <h2 name="txtAvanzar" id="txtAvanzar"></h2>
+
+        </div>
+
+        <div class="numeroDado">
+            <h2>Avanzas</h2>
+            <h2 name="txtAvanzar" id="txtAvanzar"></h2>
+        </div>
     </div>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
